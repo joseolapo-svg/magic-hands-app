35b4d833b1bd4f7c7491f0e3155b2ed75c1e9396
joseolapo-svg <jose.o.lapo@gmail.com>
2026-08-24 09:52:43 -0500

    Update supabaseClient.ts.tsx

--- supabaseClient.ts.tsx
+++ supabaseClient.ts.tsx
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
 // 1. Tu URL del proyecto
-const SUPABASE_URL = 'https://veknjtcgslgpirxiehkn.supabase.co/rest/v1/'
+const SUPABASE_URL = 'https://veknjtcgslgpirxiehkn.supabase.co'
+```[cite: 4]
 
-// 2. Tu Publishable key (asegúrate de copiarla completa desde la pantalla de API Keys)
 const SUPABASE_ANON_KEY = 'sb_publishable_EJJcC1CI_aoVUVywLPu61g_ohheSR1L' // Reemplaza los puntos suspensivos con el resto de tu clave
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
